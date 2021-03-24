abdb0314462f8e5ddd9d4ceee93779fd45783a00
Julie Nikitina <jq.nikitina@gmail.com>
2021-03-25 00:53:42 +0800

    Static pages debug2

--- about/views.py
+++ about/views.py
@@ -2,9 +2,9 @@ from django.views.generic.base import TemplateView
 
 
 class AboutAuthorView(TemplateView):
-    template_name = 'templates/about/author.html'
+    template_name = 'about/author.html'
 
 
 class AboutTechView(TemplateView):
-    template_name = 'templates/about/tech.html'
+    template_name = 'about/tech.html'
 
